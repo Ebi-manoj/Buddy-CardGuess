7dcd24a8edb4fd74b0c590010c9de892ae27a673
Ebi manoj <ebimanoj28@gmail.com>
2025-01-17 22:34:05 +0530

    card align

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-// const drawCard = async function () {
-//   try {
-//     const response = await fetch(
-//       'https://deckofcardsapi.com/api/deck/new/draw/?count=16'
-//     );
-//     if (!response.ok) throw new Error();
-//     const data = await response.json();
-//     document.body.innerHTML = `<img src="${data.cards[0].image}" alt=""></img>`;
+// deck https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+// card https://deckofcardsapi.com/api/deck/new/draw/?count=16
 
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// drawCard();
+////////////HTML ELEMENTS///////////////
+
+const cardContainer = document.querySelector('.card-display');
+const btnStart = document.querySelector('.btn-start');
+
+// card alignment
+const alignCard = function () {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+    card.style.left = `${index * 5.8}%`;
+  });
+};
+alignCard();
+
+let data;
+
+// drawing 16cards from the deck
+const drawCard = async function () {
+  try {
+    const response = await fetch(
+      'https://deckofcardsapi.com/api/deck/new/draw/?count=16'
+    );
+    if (!response.ok) throw new Error();
+    data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+drawCard();
+
+// Game starting after clicking start button
+// btnStart.addEventListener('click', function () {
+//   cardContainer.innerHTML = '';
+//   data.cards.forEach(card =>
+//     cardContainer.insertAdjacentHTML(
+//       'afterbegin',
+//       `
+//       <div class="card">
+//           <img src="${card.image}" class="img-fluid deck-img" alt="card" />
+//         </div>
+//     `
+//     )
+//   );
+//   alignCard();
+// });
